2835498dd4648a6af69e4cad35ae4bef5423bf26
ianuragsh52 <anurag.sh052@gmail.com>
2026-08-12 17:13:40 +0530

    Add Two number

---
+++ AdditionTwoNumber.java
@@ -0,0 +1,10 @@
+public class AdditionTwoNumber {
+    public static void main(String[] args) {
+        System.out.println("sum of two number");
+        int a=34;
+        int b=16;
+        int sum = a+b;
+
+        System.out.println(sum);
+    }
+}
