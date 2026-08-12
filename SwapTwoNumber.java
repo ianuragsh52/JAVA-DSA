851175d901a8dae1e60dbcac2aaaefef572c4b57
ianuragsh52 <anurag.sh052@gmail.com>
2026-08-12 17:15:22 +0530

    Swap two number

---
+++ SwapTwoNumber.java
@@ -0,0 +1,17 @@
+public class SwapTwoNumber {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 40;
+        //int c = 50;
+        int temp;
+        temp = a;
+        a = b ;
+        b = temp;
+       // temp = b ;
+      //  b = c ;
+      //  c = temp;
+        System.out.println("a=" +a );
+        System.out.println("b=" +b );
+       // System.out.println("c=" +c);
+    }
+}
